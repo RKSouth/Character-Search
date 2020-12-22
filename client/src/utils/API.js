f90ff10cf94cf8207da1237111c5fcc1cd8c3b63
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2020-12-22 12:20:42 -0800

    cleaned up api code

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,32 +5,22 @@ const API = {
         return axios.get('/characters.json')
     },
 
-    // grab the books from the database that was saved by the user
+    // grab the characters from the database that was saved by the user
     getApiChars: function() {
         return axios.get("/api/characters");
     },
-    // grab the books from the database that was saved by the user
- 
-  getCharacter: function(id) {
+    // grab the characters from the database that was saved by the user
+     getCharacter: function(id) {
     return axios.get("/api/characters/" + id);
-  },
+    },
     // add a book to the database when the user clicks save book
-
     addCharacter: function(data) {
         return axios.post("/api/characters", data);
     },
-    // delete the book by id when the user decides to remove from the saved page
-    // deleteBook: function(id) {
-    //     return axios.delete("/api/books/" + id);
-    // },
-
     deleteCharacter: function(id) {
         return axios.delete("/api/characters/" + id);
     },
-    // find a book by id
-    findBook: function(id) {
-        return axios.get("/api/search");
-    },
+    // find a character by id
     findCharacter: function(id) {
         return axios.get("/api/search");
     },
